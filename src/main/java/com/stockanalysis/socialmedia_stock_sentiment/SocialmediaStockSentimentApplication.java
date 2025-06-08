d9919f52e945e261aaf0a9926606f3398d7f520a
danish <danish>
2025-06-08 17:16:08 -0500

    basic project structure

---
+++ src/main/java/com/stockanalysis/socialmedia_stock_sentiment/SocialmediaStockSentimentApplication.java
@@ -0,0 +1,13 @@
+package com.stockanalysis.socialmedia_stock_sentiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialmediaStockSentimentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialmediaStockSentimentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stockanalysis/socialmedia_stock_sentiment/SocialmediaStockSentimentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockanalysis.socialmedia_stock_sentiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialmediaStockSentimentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
