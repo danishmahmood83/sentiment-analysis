260de489e688e770626a6ba853bd42256c00a7f2
Marla <mack7801@yahoo.com>
2025-07-11 18:44:45 -0500

    fix javadoc port

--- stock-sentiment.sh
+++ stock-sentiment.sh
@@ -9,6 +9,7 @@ cd target/site/apidocs
 python -m http.server 80
 
 # 4. Open browser to http://localhost:8080
+open "http://localhost:80"
 
 # Start Kafka Container
 docker run -p 9092:9092 apache/kafka:4.0.0
